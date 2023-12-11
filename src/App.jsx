69c691756c913bf243efad115d5288edcfe37455
dlhg <gragg.drew@gmail.com>
2023-12-11 17:04:51 -0500

    removed empty funcs

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,11 @@
 // src/App.js
-import React from 'react';
-import GameCanvas from './components/GameCanvas';
+import React from "react";
+import GameCanvas from "./components/GameCanvas";
 
 function App() {
-  const spawnBall = () => {
-    // Logic to spawn a ball
-  };
-
-  const spawnBrick = () => {
-    // Logic to spawn a brick
-  };
-
   return (
     <div>
       <GameCanvas />
-
     </div>
   );
 }
