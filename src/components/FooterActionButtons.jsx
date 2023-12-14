2d708082521384a6da3fff8e00d1932833e1d8e8
dlhg <gragg.drew@gmail.com>
2023-12-14 12:51:16 -0500

    put footer in fixed position and added placeholder button text to footer component

--- src/components/FooterActionButtons.jsx
+++ src/components/FooterActionButtons.jsx
@@ -3,9 +3,9 @@ import React from "react";
 const FooterActionButtons = ({ buyBall }) => {
   return (
     <div className="footer--action--buttons">
-      <button className="action--button">Button 1</button>
-      <button className="action--button">Button 2</button>
-      <button className="action--button">Button 3</button>
+      <button className="action--button">Balls</button>
+      <button className="action--button">Skill Tree</button>
+      <button className="action--button">Boosts</button>
       <button className="action--button">Button 4</button>
       <button className="action--button" onClick={buyBall}>
         Buy Ball(20 gems)
