f513956cceff008f993222af37a2eca346ba0fc5
dlhg <gragg.drew@gmail.com>
2023-12-14 00:24:43 -0500

    settings icon now animates when clicked

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,10 +4,19 @@ display player level
 settings
 
 */
-import React from "react";
+import React, { useState } from "react";
 import SettingsIcon from "../assets/images/UI/settingsicon.png";
 
 const Navbar = ({ gems, playerLevel }) => {
+  const [isSettingsIconClicked, setIsSettingsIconClicked] = useState(false);
+
+  const handleSettingsIconClick = () => {
+    setIsSettingsIconClicked(true);
+    setTimeout(() => {
+      setIsSettingsIconClicked(false);
+    }, 100); // This timeout duration should match the CSS transition duration
+  };
+
   return (
     <div className="navbar">
       <div className="player-info">
@@ -16,8 +25,11 @@ const Navbar = ({ gems, playerLevel }) => {
         <span> Level: {playerLevel}</span>
       </div>
       <div className="settings--icon--container">
-        <button className="settings--icon">
-          <img src={SettingsIcon} alt="Settings" />
+        <button
+          className={`settings--icon ${isSettingsIconClicked ? "clicked" : ""}`}
+          onClick={handleSettingsIconClick}
+        >
+          <img src={SettingsIcon}></img>
         </button>
       </div>
     </div>
