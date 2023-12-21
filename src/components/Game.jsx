9c53bbb63e0ffa717c44be9563c26677a9d61ef4
dlhg <gragg.drew@gmail.com>
2023-12-21 17:40:56 -0500

    adjusted brick spawn interval decrease threshold

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -613,11 +613,11 @@ function Game() {
     if (isSpawningBricks) {
       intervalId = setInterval(() => {
         if (bricks.length <= maxBricksOnScreen) {
-          if (bricks.length < 40) {
+          if (bricks.length < 50) {
             setBrickSpawnRate((prevRate) => {
               const newRate = prevRate * 0.95;
               console.log(
-                `less than 40 bricks on screen , Brick spawn interval decreased to ${newRate}`
+                `less than 50 bricks on screen , Brick spawn interval decreased to ${newRate}`
               );
               return newRate;
             });
