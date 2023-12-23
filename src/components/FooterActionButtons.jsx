190da8ae61cd88359d2f26add445ac3b09999255
dlhg <gragg.drew@gmail.com>
2023-12-22 22:07:48 -0500

    conditionally render other action buttons besides buy ball only if player has purchased a ball already

--- src/components/FooterActionButtons.jsx
+++ src/components/FooterActionButtons.jsx
@@ -1,3 +1,5 @@
+import { React, useState } from "react";
+
 const FooterActionButtons = ({
   gems,
   buyBall,
@@ -11,44 +13,53 @@ const FooterActionButtons = ({
   buyClickDamageUpgrade,
   clickDamageUpgradePrice,
 }) => {
+  const [doesPlayerHaveAtLeastOneBall, setDoesPlayerHaveAtLeastOneBall] =
+    useState(false);
   return (
     <div className="footer">
       <button
-        onClick={buyBall}
+        onClick={() => {
+          buyBall();
+          setDoesPlayerHaveAtLeastOneBall(true);
+        }}
         style={{ color: gems >= ballPrice ? "green" : "red" }}
       >
         +Ball
         <br />({ballPrice.toFixed(0)}g)
       </button>
 
-      <button
-        onClick={buyBallSpeedUpgrade}
-        style={{ color: gems >= ballSpeedUpgradePrice ? "green" : "red" }}
-      >
-        +Speed
-        <br />({ballSpeedUpgradePrice.toFixed(0)}g)
-      </button>
-      <button
-        onClick={buyBallRadiusUpgrade}
-        style={{ color: gems >= ballRadiusUpgradePrice ? "green" : "red" }}
-      >
-        +Size
-        <br />({ballRadiusUpgradePrice.toFixed(0)}g)
-      </button>
-      <button
-        onClick={buyBallDamageUpgrade}
-        style={{ color: gems >= ballDamageUpgradePrice ? "green" : "red" }}
-      >
-        +Ball Damage
-        <br />({ballDamageUpgradePrice.toFixed(0)}g)
-      </button>
-      <button
-        onClick={buyClickDamageUpgrade}
-        style={{ color: gems >= clickDamageUpgradePrice ? "green" : "red" }}
-      >
-        +Click Damage
-        <br />({clickDamageUpgradePrice.toFixed(0)}g)
-      </button>
+      {doesPlayerHaveAtLeastOneBall && (
+        <>
+          <button
+            onClick={buyBallSpeedUpgrade}
+            style={{ color: gems >= ballSpeedUpgradePrice ? "green" : "red" }}
+          >
+            +Speed
+            <br />({ballSpeedUpgradePrice.toFixed(0)}g)
+          </button>
+          <button
+            onClick={buyBallRadiusUpgrade}
+            style={{ color: gems >= ballRadiusUpgradePrice ? "green" : "red" }}
+          >
+            +Size
+            <br />({ballRadiusUpgradePrice.toFixed(0)}g)
+          </button>
+          <button
+            onClick={buyBallDamageUpgrade}
+            style={{ color: gems >= ballDamageUpgradePrice ? "green" : "red" }}
+          >
+            +Ball Damage
+            <br />({ballDamageUpgradePrice.toFixed(0)}g)
+          </button>
+          <button
+            onClick={buyClickDamageUpgrade}
+            style={{ color: gems >= clickDamageUpgradePrice ? "green" : "red" }}
+          >
+            +Click Damage
+            <br />({clickDamageUpgradePrice.toFixed(0)}g)
+          </button>
+        </>
+      )}
     </div>
   );
 };
