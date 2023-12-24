0ad891079dba2da38e20f5c3072bffdc8ddbfb14
Drew Gragg <98364811+dlhg@users.noreply.github.com>
2023-12-23 22:26:18 -0500

    vite config changes

--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-
-
 export default defineConfig({
   plugins: [react()],
-  //add the server config below
   server: {
-    host: true,
-    strictPort: true,
-    port: 3000
+    host: true, // This sets host to '0.0.0.0' to listen on all interfaces
+    strictPort: true, // Ensures the port is not automatically changed if occupied
+    port: 80 // Sets the server port, ensure this is the port you want and is allowed in your deployment environment
   }
 })
